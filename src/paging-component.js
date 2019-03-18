ce6f714f5a27298dc9a518197ecc99e8f940fa44
Tommy Tran <tranttommy95@gmail.com>
2019-03-18 16:15:49 -0700

    refactored button disabling

--- src/paging-component.js
+++ src/paging-component.js
@@ -2,7 +2,6 @@ import { writePageToQuery } from './hash-query.js';
 
 const previousButton = document.getElementById('previous-button');
 const nextButton = document.getElementById('next-button');
-const currentPage = document.getElementById('current-page');
 
 let currentPageNumber = 1;
 
@@ -11,12 +10,11 @@ previousButton.addEventListener('click', () => {
     const existingQuery = window.location.hash.slice(1);
     const newQuery = writePageToQuery(existingQuery, currentPageNumber);
     window.location.hash = newQuery;
-    currentPage.textContent = currentPageNumber;
 });
+
 nextButton.addEventListener('click', () => {
     currentPageNumber++;
     const existingQuery = window.location.hash.slice(1);
     const newQuery = writePageToQuery(existingQuery, currentPageNumber);
     window.location.hash = newQuery;
-    currentPage.textContent = currentPageNumber;
 });
