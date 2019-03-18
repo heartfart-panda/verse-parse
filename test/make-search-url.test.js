69ed364e9495e2bf07ab2668461e041717f5c5e2
Tommy Tran <tranttommy95@gmail.com>
2019-03-18 13:23:58 -0700

    returns empty string if no search

--- test/make-search-url.test.js
+++ test/make-search-url.test.js
@@ -8,6 +8,9 @@ const CORS_ANYWHERE = 'https://cors-anywhere.herokuapp.com/';
 const API_KEY = 'c50fe663a733fdddb10284d0025a7487';
 
 function makeSearchUrl(queryOptions) {
+    if(!queryOptions.search) {
+        return '';
+    }
     const url = new URL(BASE_URL + ARTIST_SEARCH);
     url.searchParams.set('q_artist', queryOptions.search);
     url.searchParams.set('apikey', API_KEY);
@@ -22,6 +25,20 @@ test('URL construction for API fetching', assert => {
     const expected = CORS_ANYWHERE + 'https://api.musixmatch.com/ws/1.1/artist.search?q_artist=kanye+west&apikey=c50fe663a733fdddb10284d0025a7487&page=1&page_size=5';
     const queryOptions = {
         search: 'kanye west',
+        page: 1
+    };
+    // Act
+    const result = makeSearchUrl(queryOptions);
+
+    // Assert
+    assert.equal(result, expected);
+});
+
+test('Return empty string if no search', assert => {
+    // Arrange
+    const expected = '';
+    const queryOptions = {
+        search: '',
         page: 1
     };
     // Act
