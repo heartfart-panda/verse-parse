7cba6e2d1bad4c15089ddc2e17549f5ef4d0048e
Tommy Tran <tranttommy95@gmail.com>
2019-03-18 15:29:02 -0700

    imported function and change page number display

--- src/paging-component.js
+++ src/paging-component.js
@@ -1,3 +1,5 @@
+import { writePageToQuery } from './hash-query.js';
+
 const previousButton = document.getElementById('previous-button');
 const nextButton = document.getElementById('next-button');
 const currentPage = document.getElementById('current-page');
@@ -9,11 +11,12 @@ previousButton.addEventListener('click', () => {
     const existingQuery = window.location.hash.slice(1);
     const newQuery = writePageToQuery(existingQuery, currentPageNumber);
     window.location.hash = newQuery;
+    currentPage.textContent = currentPageNumber;
 });
 nextButton.addEventListener('click', () => {
     currentPageNumber++;
     const existingQuery = window.location.hash.slice(1);
     const newQuery = writePageToQuery(existingQuery, currentPageNumber);
     window.location.hash = newQuery;
+    currentPage.textContent = currentPageNumber;
 });
-
