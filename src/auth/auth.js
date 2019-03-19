7d25f0881dcbba5cd10d19bc5c08b8521ba65708
Tommy Tran <tranttommy95@gmail.com>
2019-03-18 17:00:37 -0700

    added firebase sign-in

---
+++ src/auth/auth.js
@@ -0,0 +1,23 @@
+import { auth } from '../firebase.js';
+const ui = new firebaseui.auth.AuthUI(auth);
+
+ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    signInSuccessUrl: './',
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    // callbacks: {
+    //     signInSuccessWithAuthResult(authResult) {
+    //         const user = authResult.user;
+    //         usersRef.child(user.uid)
+    //             .set({
+    //                 uid: user.uid,
+    //                 displayName: user.displayName,
+    //                 photoURL: user.photoURL
+    //             });
+    //         return true;
+    //     }
+    // }
+});
