79c225726de71122c4fd6ebbc3cfeda2fa2d408b
Tommy Tran <tranttommy95@gmail.com>
2019-03-20 13:42:43 -0700

    can remove favorites from library (did not want fetch to happen like that)

--- src/favorites/favorites.js
+++ src/favorites/favorites.js
@@ -9,36 +9,38 @@ auth.onAuthStateChanged(user => {
     const userId = user.uid;
     const userFavorites = favoritesByUserRef.child(userId);
     const userLibraryRef = librariesByUserRef.child(userId);
+    let artistList = null;
     userFavorites.on('value', snapshot => {
         const value = snapshot.val();
-        const artistList = Object.values(value);
+        artistList = Object.values(value);
         loadArtists(artistList, favoritedArtistsList);
-        submitFavoriteArtistsButton.addEventListener('click', () => {
-            artistList.forEach(artist => {
-                const artistLibraryRef = userLibraryRef.child(artist.artist_id);
-                const url = makeTrackSearchUrl(artist);
-                fetch(url)
-                    .then(response => response.json())
-                    .then(trackResult => {
-                        const trackList = trackResult.message.body.track_list;
-                        trackList.forEach(track => {
-                            const trackId = track.track.track_id;
-                            const trackRef = artistLibraryRef.child(trackId);
-                            const lyricsSearchUrl = makeLyricSearchUrl(trackId);
-                            fetch(lyricsSearchUrl)
-                                .then(response => response.json())
-                                .then(lyricsResult => {
-                                    const lyrics = lyricsResult.message.body.lyrics.lyrics_body;
-                                    trackRef.set({ 
-                                        track_id: track.track.track_id,
-                                        track_name: track.track.track_name,
-                                        lyrics: lyrics.slice(0, -76),
-                                        artist_name: track.track.artist_name
-                                    });
+    });
+    submitFavoriteArtistsButton.addEventListener('click', () => {
+        userLibraryRef.remove();
+        artistList.forEach(artist => {
+            const artistLibraryRef = userLibraryRef.child(artist.artist_id);
+            const url = makeTrackSearchUrl(artist);
+            fetch(url)
+                .then(response => response.json())
+                .then(trackResult => {
+                    const trackList = trackResult.message.body.track_list;
+                    trackList.forEach(track => {
+                        const trackId = track.track.track_id;
+                        const trackRef = artistLibraryRef.child(trackId);
+                        const lyricsSearchUrl = makeLyricSearchUrl(trackId);
+                        fetch(lyricsSearchUrl)
+                            .then(response => response.json())
+                            .then(lyricsResult => {
+                                const lyrics = lyricsResult.message.body.lyrics.lyrics_body;
+                                trackRef.set({ 
+                                    track_id: track.track.track_id,
+                                    track_name: track.track.track_name,
+                                    lyrics: lyrics.slice(0, -76),
+                                    artist_name: track.track.artist_name
                                 });
-                        });
+                            });
                     });
-            });
+                });
         });
     });
 });
