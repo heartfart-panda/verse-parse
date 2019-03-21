a8775de71201d2efc413acc4e268dc8e0de5d1ab
Cheri Portman <cheriportman@hotmail.com>
2019-03-21 12:01:34 -0700

    styling!

--- src/game/game.js
+++ src/game/game.js
@@ -1,8 +1,13 @@
 import loadGame from './game-component.js';
 import { clearGame } from './game-component.js';
 import { auth, librariesByUserRef } from '../firebase.js';
+import loadHeader from '../../src/header-component.js';
+import loadProfile from '../profile-component.js';
+
 const startButton = document.getElementById('start-button');
 
+loadHeader();
+loadProfile();
 
 auth.onAuthStateChanged(user => {
     const userId = user.uid;
--- src/results/results.js
+++ src/results/results.js
@@ -1,7 +1,10 @@
 import loadProfile from '../profile-component.js';
 import { usersRef } from '../firebase.js';
 import loadScoreboard from './scoreboard-component.js';
+import loadHeader from '../header-component.js';
 
+
+loadHeader();
 loadProfile();
 
 usersRef.once('value')
